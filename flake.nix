99e0eca2e5d1aa3da8e78340e9f154edbdb77ee2
Ivan Johnson <git@IvanJohnson.net>
2025-12-27 00:41:30 -0500

    Customize flake.nix

--- flake.nix
+++ flake.nix
@@ -1,82 +1,35 @@
 {
-  description = "AVR HAL development environment";
+	description = "AVR HAL development environment";
 
-  inputs.devshell.url = "github:numtide/devshell";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.fenix.url = "github:nix-community/fenix";
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-25.11-small";
+		fenix = {
+			url = "github:nix-community/fenix";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
 
-  outputs =
-    {
-      self,
-      fenix,
-      flake-utils,
-      devshell,
-      nixpkgs,
-    }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShell =
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-
-            overlays = [ devshell.overlays.default fenix.overlays.default ];
-          };
-        in
-        pkgs.devshell.mkShell ({extraModulesPath, ...}: let
-          rust-toolchain = pkgs.fenix.fromToolchainFile {
-            file = ./rust-toolchain.toml;
-            sha256 = "DnyK5MS+xYySA+csnnMogu2gtEfyiy10W0ATmAvmjGg=";
-          };
-          c-compiler = pkgs.pkgsCross.avr.buildPackages.gcc;
-        in {
-          name = "avr-hal";
-
-          imports = [
-            "${extraModulesPath}/language/c.nix"
-          ];
-
-          language.c = {
-            compiler = c-compiler;
-          };
-
-          commands = [
-            {
-              name = "rustc";
-              category = "rust";
-              help = "Rust compiler";
-              package = rust-toolchain;
-            }
-            {
-              name = "cargo";
-              category = "rust";
-              help = "Rust build tool";
-              package = rust-toolchain;
-            }
-            {
-              name = "rustfmt";
-              category = "rust";
-              help = "Rust formatting tool";
-              package = rust-toolchain;
-            }
-            {
-              name = "avrdude";
-              category = "avr";
-              help = "Programmer for AVR chips";
-              package = pkgs.avrdude;
-            }
-            {
-              name = "ravedude";
-              category = "avr";
-              help = "Rust adapter for flashing with avrdude";
-              package = pkgs.ravedude;
-            }
-            {
-              name = "avr-gcc";
-              category = "avr";
-              help = "AVR C/C++ compiler (used for linking)";
-              package = c-compiler;
-            }
-          ];
-        });
-    });
+	outputs =
+		{
+			self,
+			nixpkgs,
+			fenix,
+		}:
+		let
+			pkgs = import nixpkgs { system = "x86_64-linux"; };
+		in
+		{
+			devShells.x86_64-linux.default = pkgs.mkShell {
+				buildInputs = [
+					pkgs.pkgsCross.avr.buildPackages.gcc
+					(pkgs.python3.withPackages (python-pkgs: with python-pkgs; [ pyserial ]))
+					pkgs.minicom
+					(fenix.packages.x86_64-linux.fromToolchainFile {
+						file = ./rust-toolchain.toml;
+						sha256 = "sha256-z8J/GH7znPPg9kKvPirKcBeXqHikj1M7KB+anwsDx0M=";
+					})
+				];
+				RAVEDUDE_PORT = "/dev/ttyACM0";
+			};
+		};
 }
