f274ad786c83f606f5c7724a2de78fa51454811e
Ivan Johnson <git@IvanJohnson.net>
2025-12-30 02:56:20 -0500

    wip

--- mcu/atmega-hal/src/usb.rs
+++ mcu/atmega-hal/src/usb.rs
@@ -482,7 +482,7 @@ impl ClearInterrupts for UDINT {
         for<'w> F: FnOnce(&mut Self::Writer) -> &mut Self::Writer,
     {
         // Bits 1,7 reserved as do not set. Setting all other bits has no effect
-        self.write(|w| f(unsafe { w.bits(0x7d) }))
+        self.write(|w| f(unsafe { w.bits(0x7d) }));
     }
 }
 
@@ -495,7 +495,7 @@ impl ClearInterrupts for UEINTX {
     {
         // Bit 5 read-only. Setting all other bits has no effect, EXCEPT:
         //  - RXOUTI/KILLBK should not be set for "IN" endpoints (XXX end-user beware)
-        self.write(|w| f(unsafe { w.bits(0xdf) }))
+        self.write(|w| f(unsafe { w.bits(0xdf) }));
     }
 }
 
@@ -507,7 +507,7 @@ impl ClearInterrupts for USBINT {
         for<'w> F: FnOnce(&mut Self::Writer) -> &mut Self::Writer,
     {
         // Bits 7:1 are reserved as do not set.
-        self.write(|w| f(unsafe { w.bits(0x01) }))
+        self.write(|w| f(unsafe { w.bits(0x01) }));
     }
 }
 
@@ -521,14 +521,14 @@ impl SuspendNotifier for () {}
 
 impl SuspendNotifier for PLL {
     fn suspend(&self) {
-        self.pllcsr.modify(|_, w| w.plle().clear_bit());
+        self.pllcsr().modify(|_, w| w.plle().clear_bit());
     }
 
     fn resume(&self) {
-        self.pllcsr
+        self.pllcsr()
             .modify(|_, w| w.pindiv().set_bit().plle().set_bit());
 
-        while self.pllcsr.read().plock().bit_is_clear() {}
+        while self.pllcsr().read().plock().bit_is_clear() {}
     }
 }
 
@@ -551,65 +551,4 @@ fn delay_cycles(cycles: u32) {
         )
     }
 }
-use avr_device::atmega32u4::PLL;
-use avr_device::atmega32u4::USB_DEVICE;
-use usb_device::bus::PollResult;
-use usb_device::bus::UsbBus;
-use usb_device::endpoint::EndpointAddress;
-use usb_device::endpoint::EndpointType;
-use usb_device::UsbDirection;
-use usb_device::UsbError;
 
-pub struct UsbdBus {}
-
-impl UsbdBus {
-	// TODO: I'm not sure that the arguments to the `new` function are
-	// correct; there's a chance that they'll need to change during
-	// implementation.
-	pub fn new(_usb: USB_DEVICE, _pll: PLL) -> Self {
-		todo!();
-	}
-}
-
-impl UsbBus for UsbdBus {
-	fn alloc_ep(
-		&mut self,
-		_: UsbDirection,
-		_: Option<EndpointAddress>,
-		_: EndpointType,
-		_: u16,
-		_: u8,
-	) -> Result<EndpointAddress, UsbError> {
-		todo!()
-	}
-	fn enable(&mut self) {
-		todo!()
-	}
-	fn reset(&self) {
-		todo!()
-	}
-	fn set_device_address(&self, _: u8) {
-		todo!()
-	}
-	fn write(&self, _: EndpointAddress, _: &[u8]) -> Result<usize, UsbError> {
-		todo!()
-	}
-	fn read(&self, _: EndpointAddress, _: &mut [u8]) -> Result<usize, UsbError> {
-		todo!()
-	}
-	fn set_stalled(&self, _: EndpointAddress, _: bool) {
-		todo!()
-	}
-	fn is_stalled(&self, _: EndpointAddress) -> bool {
-		todo!()
-	}
-	fn suspend(&self) {
-		todo!()
-	}
-	fn resume(&self) {
-		todo!()
-	}
-	fn poll(&self) -> PollResult {
-		todo!()
-	}
-}
