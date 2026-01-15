be62c399b2b25410d2185f7f7d7a8d65cabc7514
Ivan Johnson <git@IvanJohnson.net>
2026-01-14 20:02:14 -0500

    Formatter

--- examples/arduino-micro/src/bin/std_stub.rs
+++ examples/arduino-micro/src/bin/std_stub.rs
@@ -6,19 +6,19 @@ use arduino_hal::{delay_ms, pins, Peripherals};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    let dp = unsafe { Peripherals::steal() };
-    let pins = pins!(dp);
-    let mut status = pins.d13.into_output();
-    loop {
-        status.set_high();
-        delay_ms(100);
-        status.set_low();
-        delay_ms(100);
-        status.set_high();
-        delay_ms(300);
-        status.set_low();
-        delay_ms(500);
-    }
+	let dp = unsafe { Peripherals::steal() };
+	let pins = pins!(dp);
+	let mut status = pins.d13.into_output();
+	loop {
+		status.set_high();
+		delay_ms(100);
+		status.set_low();
+		delay_ms(100);
+		status.set_high();
+		delay_ms(300);
+		status.set_low();
+		delay_ms(500);
+	}
 }
 
 #[lang = "eh_personality"]
